21b649640be7b7aa3d7a818a426914217685cf15
Ahmed Bichiou <eunabs@outlook.fr>
2024-10-15 18:00:48 +0100

    Initial commit

---
+++ src/layout/AppTopbar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { useLayout } from '@/layout/composables/layout';
+
+const { onMenuToggle, toggleDarkMode, isDarkTheme } = useLayout();
+</script>
+
+<template>
+    <div class="layout-topbar">
+        <div class="layout-topbar-logo-container">
+            <button class="layout-menu-button layout-topbar-action" @click="onMenuToggle">
+                <i class="pi pi-bars"></i>
+            </button>
+            <router-link to="/" class="layout-topbar-logo">
+
+
+                
+            </router-link>
+        </div>
+
+        <div class="layout-topbar-actions">
+            <div class="layout-config-menu">
+                <button type="button" class="layout-topbar-action" @click="toggleDarkMode">
+                    <i :class="['pi', { 'pi-moon': isDarkTheme, 'pi-sun': !isDarkTheme }]"></i>
+                </button>
+           
+                <div class="p-card  rounded-card">
+            <button type="button" class="p-button p-button-rounded" @click="$router.push('/pages/crud')">
+                <span>Services</span>
+            </button>
+        </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+</template>
