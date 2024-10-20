d385f9ded0daa6c9d60c1d3a068b18f18d0e5df7
Ahmed Bichiou <eunabs@outlook.fr>
2024-10-20 15:38:22 +0100

    fixed layouts of confirm page

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,21 +1,13 @@
 <script setup>
 import { useLayout } from '@/layout/composables/layout';
-import { computed, ref, watch } from 'vue';
+import { computed} from 'vue';
 import AppFooter from './AppFooter.vue';
-import AppSidebar from './AppSidebar.vue';
 import AppTopbar from './AppTopbar.vue';
 
-const { layoutConfig, layoutState, isSidebarActive, resetMenu } = useLayout();
+const { layoutConfig, layoutState} = useLayout();
 
-const outsideClickListener = ref(null);
 
-watch(isSidebarActive, (newVal) => {
-    if (newVal) {
-        bindOutsideClickListener();
-    } else {
-        unbindOutsideClickListener();
-    }
-});
+
 
 const containerClass = computed(() => {
     return {
@@ -27,43 +19,23 @@ const containerClass = computed(() => {
     };
 });
 
-function bindOutsideClickListener() {
-    if (!outsideClickListener.value) {
-        outsideClickListener.value = (event) => {
-            if (isOutsideClicked(event)) {
-                resetMenu();
-            }
-        };
-        document.addEventListener('click', outsideClickListener.value);
-    }
-}
 
-function unbindOutsideClickListener() {
-    if (outsideClickListener.value) {
-        document.removeEventListener('click', outsideClickListener);
-        outsideClickListener.value = null;
-    }
-}
 
-function isOutsideClicked(event) {
-    const sidebarEl = document.querySelector('.layout-sidebar');
-    const topbarEl = document.querySelector('.layout-menu-button');
 
-    return !(sidebarEl.isSameNode(event.target) || sidebarEl.contains(event.target) || topbarEl.isSameNode(event.target) || topbarEl.contains(event.target));
-}
+
 </script>
 
 <template>
     <div class="layout-wrapper" :class="containerClass">
         <app-topbar></app-topbar>
-        <app-sidebar></app-sidebar>
+        
         <div class="layout-main-container">
             <div class="layout-main">
                 <router-view></router-view>
             </div>
             <app-footer></app-footer>
         </div>
-        <div class="layout-mask animate-fadein"></div>
+        
     </div>
     <Toast />
 </template>
